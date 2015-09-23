d6b6b1b9dafa8078e6383e49e1ac463cc4a6bf6d
Hidetaka Okamoto <hide.okamoto@digitalcube.jp>
2015-09-23 23:27:46 +0900

    Add container

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="hfeed site container">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'zuiho' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
